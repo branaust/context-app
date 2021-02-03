b9284e550c4edef7b896fd53c8461061e54afd2e
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-02-03 09:07:09 -0800

    Styles

---
+++ src/Navbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles'
+import Switch from "@material-ui/core/Switch"
+import styles from './styles/NavbarStyles'
+import SearchIcon from '@material-ui/icons/Search'
+import InputBase from '@material-ui/core/InputBase'
+
+
+class Navbar extends Component {
+
+
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <span>🚩</span>
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title} color='inherit'>
+                            App Title
+                        </Typography>
+                        <Switch />
+                        <div className={classes.grow} />
+                        <div className={classes.search} >
+                            <div className={classes.searchIcon} >
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput
+                            }} />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+
+}
+
+
+export default withStyles(styles)(Navbar)
